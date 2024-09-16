1757204b2d4f7fe519d4cc1d39ddb7430e436aca
lsh2200 <121371582+LSH160981@users.noreply.github.com>
2024-09-16 16:45:59 +0800

    "merit: 代码重构"

--- src/utils/ZeroWidthChars.js
+++ src/utils/ZeroWidthChars.js
@@ -1,21 +1,28 @@
-// 这个文件是 有关 零宽字符的处理
+// 这个文件是有关零宽字符的处理
 
-// 判断 字符串中有没有零宽字符 有返回true 没有返回false
-export const CheckZeroWidthChars = (str) => {
-    // 正则表达式匹配常见的零宽字符
-    const Pattern = /[\u200B-\u200D\uFEFF\u2060]/g;
-    return Pattern.test(str);
-};
+// 定义零宽字符的正则表达式和默认值
+const ZERO_WIDTH_CHARS_PATTERN = /[\u200B-\u200D\uFEFF\u2060]/g;
+const DEFAULT_ZERO_WIDTH_CHAR = '\u200B';
 
+/**
+ * 判断字符串中有没有零宽字符
+ * @param {string} str - 需要检查的字符串
+ * @returns {boolean} - 有零宽字符返回true，没有返回false
+ */
+export const CheckZeroWidthChars = (str) => ZERO_WIDTH_CHARS_PATTERN.test(str);
 
-// 移除字符串中的零宽字符
-export const RemoveZeroWidthChars = (str) => {
-    // 正则表达式匹配常见的零宽字符
-    const zeroWidthCharsPattern = /[\u200B-\u200D\uFEFF\u2060]/g;
-    return str.replace(zeroWidthCharsPattern, "");
-}
+/**
+ * 移除字符串中的零宽字符
+ * @param {string} str - 需要处理的字符串
+ * @returns {string} - 移除零宽字符后的字符串
+ */
+export const RemoveZeroWidthChars = (str) => str.replace(ZERO_WIDTH_CHARS_PATTERN, "");
 
-// 给字符串添加(头部开始) 零宽字符
-export const AddZeroWidthChars = (str, ZeroWidthChars = '\u200B') => {
-    return ZeroWidthChars + str
-}
+/**
+ * 给字符串添加零宽字符
+ * @param {string} str - 需要添加零宽字符的字符串
+ * @param {string} zeroWidthChars - 要添加的零宽字符，默认为DEFAULT_ZERO_WIDTH_CHAR
+ * @returns {string} - 添加零宽字符后的字符串
+ */
+export const AddZeroWidthChars = (str, zeroWidthChars = DEFAULT_ZERO_WIDTH_CHAR) =>
+    zeroWidthChars + str;
